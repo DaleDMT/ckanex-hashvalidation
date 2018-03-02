199ecf28476c02448504707f71e2b9a14b8f8874
DaleDMT <daledmt@gmail.com>
2018-03-02 16:11:40 +0300

    Added custom resource uploader

--- ckanext/hashvalidation/plugin.py
+++ ckanext/hashvalidation/plugin.py
@@ -1,17 +1,19 @@
 # encode: utf-8
 
-from hashlib import md5
+import hashlib
+import os
 
 import ckan.plugins as p
 import ckan.plugins.toolkit as tk
-from ckan.lib.uploader import ResourceUpload
+from ckan.lib.uploader import ResourceUpload as DefaultResourceUpload
+from ckan.lib.helpers import flash_error
 
 
-def md5(fname):
-    hash_md5 = md5()
-    with open(fname, "rb") as f:
-        for chunk in iter(lambda: f.read(65536), b""):
-            hash_md5.update(chunk)
+def get_file_hash(f):
+    hash_md5 = hashlib.md5()
+    # with fname as f:
+    for chunk in iter(lambda: f.read(65536), b""):
+        hash_md5.update(chunk)
     return hash_md5.hexdigest()
 
 
@@ -21,8 +23,10 @@ def hash_validator(key, data, errors, context):
     # print 'context >>>>>>>> : {}'.format(flatten_data[key])
     print "GET >>>>>>> {}".format(data.get(key, ), None)
     print "GET >>>>>>> {}".format(data.get((key[0], key[1], 'name',)), None)
-    if data[key] != 'xxx':
-        raise tk.Invalid('Hash Invalid!')
+    # resource_create = tk.get_action('resource_create')(context, data)
+    # print 'ACTION {}'.format(resource_create)
+    # if data[key] != 'xxx':
+    #     raise tk.Invalid('Hash Invalid!')
 
 
 class HashvalidationPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
@@ -32,7 +36,16 @@ class HashvalidationPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
 
     def get_resource_uploader(self, data_dict):
         print 'UPLOAD >>>>>> {}'.format(data_dict)
-        return ResourceUpload(data_dict)
+        try:
+            return ResourceUpload(data_dict)
+        except ValueError:
+            print 'ERROR <<<<<<<<<<<<<<<<<<<< '
+            flash_error('File hash invalid')
+        return None
+
+    # def get_uploader(self, data_dict):
+    #     print 'UPLOAD2 >>>>>> {}'.format(data_dict)
+    #     return ResourceUpload(data_dict)
 
     def get_path(self, id):
         directory = self.get_directory(id)
@@ -67,4 +80,14 @@ class HashvalidationPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
         # that CKAN will use this plugin's custom templates.
         tk.add_template_directory(config, 'templates')
 
+
+class ResourceUpload(DefaultResourceUpload):
+
+    def __init__(self, resource):
+        super(ResourceUpload, self).__init__(resource)
+        print 'MD >>>>>>>>>> {}'.format(get_file_hash(self.upload_file))
+        self.file_hash = get_file_hash(self.upload_file)
+        if self.file_hash != 'xxx':
+            raise ValueError('Hash Invalid!')
+            print 'FILE: {}'.format(self.file_name)
 
